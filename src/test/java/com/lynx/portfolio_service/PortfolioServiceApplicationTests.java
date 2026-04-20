5bb6dc05ab930a628a655b855f7f200cbdfe1b33
Daniel Tordai <danieltordai@yahoo.com>
2026-04-21 00:16:23 +0300

    initial project setup

---
+++ src/test/java/com/lynx/portfolio_service/PortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynx.portfolio_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
